69e4b36673b636d2cbb3617b453988b8418a2395
paceaux <frank.m.taylor08@gmail.com>
2020-08-03 11:59:07 -0500

    creates indexes to automagically export

---
+++ gulpfile.js/tasks/index.js
@@ -0,0 +1,12 @@
+/**
+* DO NOT MODIFY. See Readme for details.
+*/
+const taskFiles = require('require-dir')();
+
+const tasks = {};
+
+Object.values(taskFiles).forEach((exportedObject) => {
+  Object.assign(tasks, exportedObject);
+});
+
+module.exports = tasks;
