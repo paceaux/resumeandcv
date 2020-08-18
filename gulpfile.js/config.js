9850fc37442f90385c664ee3c80a11119623d62e
paceaux <frank.m.taylor08@gmail.com>
2020-08-18 10:56:18 -0500

    changes sitemap generation time

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -49,7 +49,7 @@ module.exports = {
         src: `${dest}/**/*.html`,
         opts: {
             siteUrl: 'http://frankmtaylor.com',
-            changefreq: 'weekly',
+            changefreq: 'monthly',
         },
     },
 
